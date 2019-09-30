e41f973df5e2ed6c7fde782cff3c5be2fb048772
Serega <kamserg1608@gmail.com>
2019-09-30 18:44:06 +0300

    selenium grit

--- src/test/java/ru/qa/armada/n02_appManagerForTest/workWithDriver/SingletonWebDriver.java
+++ src/test/java/ru/qa/armada/n02_appManagerForTest/workWithDriver/SingletonWebDriver.java
@@ -2,9 +2,14 @@ package ru.qa.armada.n02_appManagerForTest.workWithDriver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * This class is used as a singleton to work with the WebDriver
@@ -20,7 +25,7 @@ public class SingletonWebDriver {
   /**
    *  Driver Initialization Selection
    */
-  private SingletonWebDriver(){
+  private SingletonWebDriver() {
 //    If the path to .exe in the system variable not the specified, then is necessary uncomment this line
 //    System.setProperty("webdriver.gecko.driver", "C:\\...\\ArmadaTests\\drivers\\\geckodriver.exe")
 //    System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
@@ -31,7 +36,15 @@ public class SingletonWebDriver {
         driver = new FirefoxDriver();
         break;
       case BrowserType.CHROME:
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        String  chromeBrowserProfile = "user-data-dir=C:\\Users\\Cont\\AppData\\Local\\Google\\Chrome\\User Data";
+        options.addArguments(chromeBrowserProfile);
+        String hubUrl  = "http://172.16.68.128:4444/wd/hub";
+        try {
+          driver = new RemoteWebDriver(new URL(hubUrl), options);
+        } catch (MalformedURLException e) {
+          e.printStackTrace();
+        }
         break;
       case BrowserType.IE:
         driver = new InternetExplorerDriver();
